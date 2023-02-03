b8b7251c3bb362ed5a5061c0742dc4fb2bfee512
rinarose3 <ryzhkova_ie@mail.ru>
2023-02-03 22:59:55 +0300

    Главный класс для запуска графического интерфейса

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.github.rinarose3.trafficLight;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SimpleGUI app = new SimpleGUI();
+        app.setVisible(true);
+    }
+}
